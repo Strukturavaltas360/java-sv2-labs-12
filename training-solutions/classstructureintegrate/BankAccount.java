cd11a232ffba3203004da4d7613a8b3dbd1672ae
ht0m <hartht@gmail.com>
2021-10-17 18:21:30 +0200

    Classes  and constructor created

---
+++ training-solutions/classstructureintegrate/BankAccount.java
@@ -0,0 +1,16 @@
+package classstructureintegrate;
+
+public class BankAccount {
+    private String accountNumber;
+    private String owner;
+    private int balance;
+
+    public BankAccount(String accountNumber, String owner, int balance) {
+        this.accountNumber = accountNumber;
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+
+
+}
