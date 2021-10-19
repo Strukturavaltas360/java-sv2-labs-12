1bfb58cb271e89fbf534405babc89d876fc5f5bc
ht0m <hartht@gmail.com>
2021-10-19 14:14:59 +0200

    Tests

---
+++ training-solutions/Statements/StatementMain.java
@@ -0,0 +1,24 @@
+package Statements;
+
+public class StatementMain {
+
+    public static void main(String[] args) {
+
+        int x = 5+6;
+        int y = 11-x;
+
+        int z = 8;
+        boolean b = x>y;
+        boolean c = b || z<5;
+
+        z+=1;
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+
+}
