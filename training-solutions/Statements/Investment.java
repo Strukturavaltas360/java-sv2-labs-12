3acd73ec59f6725ad5e49c15db8ce2ff6decd31c
ht0m <hartht@gmail.com>
2021-10-19 22:26:22 +0200

    Investment+InvestmentMain Created

---
+++ training-solutions/Statements/Investment.java
@@ -0,0 +1,39 @@
+package Statements;
+
+public class Investment {
+
+    private double cost;
+    private int fund;
+    private int interestRate;
+    private boolean active;
+
+    public Investment (int fund, int interestRate){
+        this.fund = fund;
+        this.interestRate = interestRate;
+        active = true;
+        cost = 0.3;
+    }
+
+    public int getFund() {
+        return fund;
+    }
+
+    public double getYield(int days) {
+        double doubleInterestRate;
+        doubleInterestRate = interestRate;
+        return getFund() * days * doubleInterestRate/36500;
+
+    }
+
+    public double close(int days) {
+        double returnInvestment;
+        returnInvestment= active == true? (getFund()+getYield(days))*(1-cost/100) : 0;
+
+        active = false;
+        return returnInvestment;
+        
+    }
+
+
+
+}
---
+++ training-solutions/Statements/InvestmentMain.java
@@ -0,0 +1,27 @@
+package Statements;
+
+import java.util.Scanner;
+
+public class InvestmentMain {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int fund;
+        int rate;
+
+        System.out.println("Befektetés összege:");
+        fund = scanner.nextInt();
+        System.out.println("Kamatláb:");
+        rate = scanner.nextInt();
+
+        Investment investment = new Investment(fund, rate);
+
+        System.out.println(investment.getFund());
+        System.out.println("Hozam 50 napra: "+ investment.getYield(50));
+        System.out.println("Kivett összeg 80 nap után: " + investment.close(80));
+        System.out.println("Kivett összeg 90 nap után: " + investment.close(80));
+    }
+
+}
