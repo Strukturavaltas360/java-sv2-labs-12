87ba954884d15fbfbc588cb5114d3684ac3d95be
ht0m <hartht@gmail.com>
2021-10-20 18:51:47 +0200

    UserValidator updated

--- training-solutions/stringtype/registration/UserValidator.java
+++ training-solutions/stringtype/registration/UserValidator.java
@@ -14,14 +14,7 @@ public class UserValidator {
     public boolean isValidEmail(String email) {
         int atPosition = email.indexOf("@");
         int dotPosition = email.indexOf(".");
-        System.out.println(atPosition);
-        System.out.println(dotPosition);
         return atPosition >= 1  && dotPosition > (atPosition+1) && dotPosition <= email.length()-2 ? true : false;
 
-        /*Az email címben kell lennie @ karakternek és valamikor utána (de nem közvetlenül) pontnak.
-        A @ karakter nem lehet az első, az őt követő pont pedig az utolsó.
-*/
-
     }
-
 }
