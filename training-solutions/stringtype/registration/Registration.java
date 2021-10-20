bb4ceb8bdb1c8b868c9e6522179369d90d163568
ht0m <hartht@gmail.com>
2021-10-20 18:52:08 +0200

    Registration updated

--- training-solutions/stringtype/registration/Registration.java
+++ training-solutions/stringtype/registration/Registration.java
@@ -11,24 +11,31 @@ public class Registration {
         String password1;
         String password2;
         String email;
+        boolean isUsernameOk;
+        boolean isPasswordOk;
+        boolean isEmailOk;
 
         Scanner sc = new Scanner(System.in);
         UserValidator uservalidator = new UserValidator();
-//
-//        username = sc.nextLine();
-//        System.out.println(username);
-//        System.out.println(uservalidator.isValidUserName(username));
-//
-//        password1 = sc.nextLine();
-//        password2 = sc.nextLine();
-//
-//        System.out.println(password1);
-//        System.out.println(password2);
-//        System.out.println(uservalidator.isValidPassword(password1,password2));
 
+        System.out.println("Regisztráció");
+        System.out.println("Kérlek add meg a felhasználónevedet!");
+        username = sc.nextLine();
+        isUsernameOk=uservalidator.isValidUserName(username);
+        System.out.println(isUsernameOk);
+
+        System.out.println("Kérlek add meg a jelszavadat!");
+        password1 = sc.nextLine();
+        System.out.println("Kérlek ismételd meg jelszavadat!");
+        password2 = sc.nextLine();
+
+        isPasswordOk=uservalidator.isValidPassword(password1,password2);
+        System.out.println(isPasswordOk);
+
+        System.out.println("Kérlek add meg az emailcímed!");
         email = sc.nextLine();
-        System.out.println(email);
-        System.out.println(uservalidator.isValidEmail(email));
+        isEmailOk=uservalidator.isValidEmail(email);
+        System.out.println(isEmailOk);
 
     }
 }
