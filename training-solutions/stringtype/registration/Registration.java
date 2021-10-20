ede736e8d5f254270265c165c8d2810e7ebe366d
ht0m <hartht@gmail.com>
2021-10-20 19:15:18 +0200

    Registration updated with messages

--- training-solutions/stringtype/registration/Registration.java
+++ training-solutions/stringtype/registration/Registration.java
@@ -21,21 +21,26 @@ public class Registration {
         System.out.println("Regisztráció");
         System.out.println("Kérlek add meg a felhasználónevedet!");
         username = sc.nextLine();
-        isUsernameOk=uservalidator.isValidUserName(username);
-        System.out.println(isUsernameOk);
+        isUsernameOk = uservalidator.isValidUserName(username);
 
-        System.out.println("Kérlek add meg a jelszavadat!");
+        System.out.println("Kérlek add meg a jelszavadat! (minimum 8 karakter hosszúságú)");
         password1 = sc.nextLine();
         System.out.println("Kérlek ismételd meg jelszavadat!");
         password2 = sc.nextLine();
 
-        isPasswordOk=uservalidator.isValidPassword(password1,password2);
-        System.out.println(isPasswordOk);
+        isPasswordOk = uservalidator.isValidPassword(password1,password2);
 
         System.out.println("Kérlek add meg az emailcímed!");
         email = sc.nextLine();
-        isEmailOk=uservalidator.isValidEmail(email);
-        System.out.println(isEmailOk);
+        isEmailOk = uservalidator.isValidEmail(email);
+
+        System.out.println();
+        System.out.println(isUsernameOk ? "* Felhasználónév: OK" : "* A felhasználónév hiányzik");
+        System.out.println(isPasswordOk ? "* Jelszó OK" : "* Hibás jelszó");
+        System.out.println(isEmailOk ? "* Email OK" : "* Hibás emailcím");
+
+        System.out.println();
+        System.out.println(isEmailOk && isPasswordOk && isUsernameOk ? "Köszönjük a regisztrációt!" : "Sajnos meg kell ismételned a regisztrációt!");
 
     }
 }
