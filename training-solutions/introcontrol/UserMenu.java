56bb4f98c87d52b281c64d01b98e4400724950a3
ht0m <hartht@gmail.com>
2021-10-22 09:54:58 +0200

    3 operandusúval és if-fel

--- training-solutions/introcontrol/UserMenu.java
+++ training-solutions/introcontrol/UserMenu.java
@@ -14,18 +14,21 @@ public class UserMenu {
         int i = scanner.nextInt();
 
         // Háromoperandusúval
-        System.out.println("Háromoperandusúval");
-        System.out.println(i == 1? "Felhasználók listázása" : (i == 2 ? "Felhasználó felvétele" : ""));
+        System.out.println("Megoldás háromoperandusúval: ");
+        System.out.println(i == 1 ? "Felhasználók listázása" : (i == 2 ? "Felhasználó felvétele" : ""));
 
         // if-fel
-        System.out.println("If-fel");
-  
+        System.out.println("Megoldás If-fel:");
+
         if (i == 1) {
             System.out.println("Felhasználók listázása");
-        } else  {
+        } else {
             if (i == 2) {
                 System.out.println("Felhasználó felvétele");
             } else {
                 System.out.println("");
+            }
+        }
     }
 }
+
