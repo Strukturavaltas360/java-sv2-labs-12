0682c442fe48ac5721937beb37594c7cae31cc11
ht0m <hartht@gmail.com>
2021-10-22 12:27:33 +0200

    More readable version

--- training-solutions/introcontrol/BoatRental.java
+++ training-solutions/introcontrol/BoatRental.java
@@ -14,18 +14,29 @@ public class BoatRental {
         head = scanner.nextInt();
         int seats=10;
 
-        if (head >= 5) { head -= 5; seats -= 5;
-            System.out.println("5 személyes csónak megtelt, további szabad férőhelyek száma: "+seats+".");}
+        if (head >= 5) {
+            head -= 5;
+            seats -= 5;
+            System.out.println("5 személyes csónak megtelt, további szabad férőhelyek száma: "+seats+".");
+        }
 
-        if (head >= 3) { head -= 3; seats -= 3;
-            System.out.println("3 személyes csónak megtelt, további szabad férőhelyek száma: "+seats+".");}
+        if (head >= 3) {
+            head -= 3;
+            seats -= 3;
+            System.out.println("3 személyes csónak megtelt, további szabad férőhelyek száma: "+seats+".");
+        }
 
-        if (head >= 2) { head -= 2; seats -= 2;
-            System.out.println("2 személyes csónak megtelt, további szabad férőhelyek száma: "+seats+".");}
+        if (head >= 2) {
+            head -= 2;
+            seats -= 2;
+            System.out.println("2 személyes csónak megtelt, további szabad férőhelyek száma: "+seats+".");
+        }
 
-        if (head == 1) { head --; seats -= 2;
-            System.out.println("2 személyes csónak félig üresen fut ki. További szabad férőhelyek száma: "+seats+".");}
-
+        if (head == 1) {
+            head --;
+            seats -= 2;
+            System.out.println("2 személyes csónak félig üresen fut ki. További szabad férőhelyek száma: "+seats+".");
+        }
 
         System.out.println("További szabad férőhelyek száma: "+seats+".");
         System.out.println("Parton maradtak száma: " + head+".");
