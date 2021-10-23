dd9732952afd2eee0ca2031785dc03a0bc131211
ht0m <hartht@gmail.com>
2021-10-23 12:40:39 +0200

    Changed variables names

--- training-solutions/arrayofarrays/Triangular.java
+++ training-solutions/arrayofarrays/Triangular.java
@@ -4,18 +4,17 @@ public class Triangular {
 
     public int[][] triangularMatrix(int size) {
 
-        int [][] a = new int[size][];
+        int [][] triangular = new int[size][];
 
             for (int i=0; i<size; i++) {
-                a[i]= new int[i+1];
+                triangular[i]= new int[i+1];
                 for (int j=0 ;j<=i ;j++) {
-                    a[i][j]=i;
-                    System.out.print(a[i][j]+" ");
+                    triangular[i][j]=i;
+                    System.out.print(triangular[i][j]+" ");
                 }
                 System.out.println();
-                
             }
-            return a;
+            return triangular;
         }
 
     public static void main(String[] args) {
