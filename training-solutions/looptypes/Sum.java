060d76051d24d0b39bbc5152f5184b0077ae064b
ht0m <hartht@gmail.com>
2021-10-24 09:16:29 +0200

    Everythird task

--- training-solutions/looptypes/Sum.java
+++ training-solutions/looptypes/Sum.java
@@ -7,7 +7,7 @@ public class Sum {
             System.out.print(numbers[i-1]+numbers[i]+" ");
         }
     }
-    
+
     public static void main(String[] args) {
         Sum sum = new Sum();
         sum.printSums(new int [] {2,6,3,5,7,9});
