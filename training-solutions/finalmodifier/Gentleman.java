1ba020de7d3ebf42dfb7372ef430cef0795fb716
ht0m <hartht@gmail.com>
2021-10-24 15:01:22 +0200

    Gentleman task

---
+++ training-solutions/finalmodifier/Gentleman.java
@@ -0,0 +1,17 @@
+package finalmodifier;
+
+public class Gentleman {
+
+
+    public String sayHello(String name) {
+        final String MESSAGE_PREFIX = "Pálinkás jóreggeét, kedves ";
+        return (MESSAGE_PREFIX + name);
+    }
+
+    public static void main(String[] args) {
+        Gentleman gentleman = new Gentleman();
+        System.out.println(gentleman.sayHello("Péter")+"!");
+
+
+    }
+}
