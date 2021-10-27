cc1b30d9d02384e959ac6ae5ca3bbfb3cf71359c
ht0m <hartht@gmail.com>
2021-10-27 11:34:13 +0200

    Testing Realestates.com

--- training-solutions/realestate/RealEstateDotCom.java
+++ training-solutions/realestate/RealEstateDotCom.java
@@ -1,5 +1,23 @@
 package realestate;
 
+
 public class RealEstateDotCom {
-    
+
+    public static void main(String[] args) {
+        Address address = new Address("Pécs", "7621", "Jókai", 2);
+        Details details = new Details("Ékszerdoboz a város szívében", address,34,0);
+        RealEstate realEstate = new RealEstate("Lakás a belvárosban",9620000, details);
+
+        System.out.println(realEstate);
+
+        Address address2 = new Address("Pécs", "7621", "Király", 31);
+        RealEstate realEstate2 = new RealEstate("Családi fészek a belvárosban",7660666, details);
+        System.out.println(realEstate2.getDetails());
+        System.out.println(realEstate2.getDetails().getAddress().street);
+
+        realEstate.getDetails().setAddress(address2);
+        System.out.println(details.getAddress().street);
+
+
+    }
 }
