cbb4df999fcda0933053c96b3c15dde0f2a46730
ht0m <hartht@gmail.com>
2021-10-30 18:34:21 +0200

    Mathoperations task

---
+++ training-solutions/numbers/MathOperations.java
@@ -0,0 +1,14 @@
+package numbers;
+
+import java.util.Scanner;
+
+public class MathOperations {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Mennyi lesz (6+5)*(7-2)/8 eredménye? ");
+        double input = scanner.nextDouble();
+        System.out.print(Math.abs(input-6.875)<0.0001 ? "A kapott megoldás helyes (±0.0001 eltérés engedélyezett)." : "A kapott megoldás minimum 0.0001-gyel eltér a helyes eredménytől.");
+    }
+}
