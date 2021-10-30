9b7894b5fef9116f1fdc11d3448f424d447f8459
ht0m <hartht@gmail.com>
2021-10-30 11:05:22 +0200

    getRightNumbers method added

--- training-solutions/mathproblems/Division.java
+++ training-solutions/mathproblems/Division.java
@@ -1,17 +1,31 @@
 package mathproblems;
 
+import java.sql.Array;
+
 public class Division {
 
     public void getDivisors(int number) {
-        System.out.print("A "+number+" osztói: ");
+        System.out.print("A "+number+" osztói: |");
         for (int i = 1; i<= number;i++) {
             System.out.print(number%i ==0 ? i+" | "  : "");
+        }
+        System.out.println();
+    }
+
+    public void getRightNumbers(int[] numbers){  
+        for (int i =1; i<numbers.length;i++) {
+            System.out.print(numbers[i]%i ==0 ? numbers[i]+" | ":"");
+
         }
     }
 
     public static void main(String[] args) {
         Division div = new Division();
+
         div.getDivisors(72);
+
+        int [] numbers = {2,4,6,7,8,9,10,14};
+        div.getRightNumbers(numbers);
     }
 
 }
