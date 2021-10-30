20a502d19a9bfcb25b32cc551b37a5009798c1d6
ht0m <hartht@gmail.com>
2021-10-30 19:37:43 +0200

    Text changed

--- training-solutions/numbers/MathOperations.java
+++ training-solutions/numbers/MathOperations.java
@@ -9,6 +9,6 @@ public class MathOperations {
 
         System.out.print("Mennyi lesz (6+5)*(7-2)/8 eredménye? ");
         double input = scanner.nextDouble();
-        System.out.print(Math.abs(input-6.875)<0.0001 ? "A kapott megoldás helyes (±0.0001 eltérés engedélyezett)." : "A kapott megoldás minimum 0.0001-gyel eltér a helyes eredménytől.");
+        System.out.print(Math.abs(input-6.875)<0.0001 ? "A kapott megoldás helyes (±0.0001 eltérés engedélyezett)." : "A kapott megoldás hibás! Legalább 0.0001-gyel eltér a helyes eredménytől.");
     }
 }
