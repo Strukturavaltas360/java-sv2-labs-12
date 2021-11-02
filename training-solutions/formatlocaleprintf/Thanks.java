23f9a75f0113f92ac304cfadea41139c8f5b38c4
ht0m <hartht@gmail.com>
2021-11-02 07:03:53 +0100

    Printf - thanks task

--- training-solutions/formatlocaleprintf/Thanks.java
+++ training-solutions/formatlocaleprintf/Thanks.java
@@ -8,7 +8,7 @@ public class Thanks {
 
     public static void main(String[] args) {
 
-        List<String > names = Arrays.asList("Molná Péter", "Kiss Ramóna", "Madár Péter");
+        List<String > names = Arrays.asList("Ábel Zsuzsanna", "Molnár Péter", "Kiss Virág", "Madár Péter");
 
         for (String s: names) {
             System.out.printf("Kedves %s! Örülünk, hogy termékünket választotta!\n", s);
