e564ec68397037dda19510e0478f6d4f304ddbed
ht0m <hartht@gmail.com>
2021-11-02 06:18:46 +0100

    Scanner task

--- training-solutions/stringscanner/StringScanner.java
+++ training-solutions/stringscanner/StringScanner.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class StringScanner {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter first sentence!");
         String sentence1 = sc.nextLine();
 
         Scanner word = new Scanner(sentence1);
@@ -12,6 +14,7 @@ public class StringScanner {
             System.out.println(word.next());
         }
 
+        System.out.println("Please enter the second sentence, with more taglines!");
         String sentence2 = sc.nextLine();
         Scanner tagLine = new Scanner(sentence1).useDelimiter(",");
         while (tagLine.hasNext()) {
