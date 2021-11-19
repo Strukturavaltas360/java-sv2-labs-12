1c976d0330b95913c193c8786b71cf391c0bb953
ht0m <hartht@gmail.com>
2021-11-19 14:56:07 +0100

    words with exceptions task

--- training-solutions/src/test/java/introexceptionreadfiletestjunit4/WordsTest.java
+++ training-solutions/src/test/java/introexceptionreadfiletestjunit4/WordsTest.java
@@ -13,14 +13,7 @@ class WordsTest {
 
     private Words words = new Words();
 
-    @Test
-    void testWordsReadFile() {
-
-        List<String> file = words.readFile(Paths.get("src/test/Resources/words.txt"));
-        assertEquals(Arrays.asList("alma", "körte", "Anna", "barack", "András", "szilva"), file);
-    }
-
-    @Test
+     @Test
     void testGetFirstWordWithA() {
         Path path = Paths.get("src/test/Resources/words.txt");
         assertEquals("Anna", words.getFirstWordWithA(path));
