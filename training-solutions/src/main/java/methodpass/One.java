71c7cd93490fa10bc8e59309f18b0894e160d1b0
ht0m <hartht@gmail.com>
2021-11-28 19:17:05 +0100

    Methodpass - WindowOperation task

---
+++ training-solutions/src/main/java/methodpass/One.java
@@ -0,0 +1,24 @@
+package methodpass;
+
+import com.sun.tools.javac.Main;
+
+public class One {
+
+
+    public static void main(String[] args) {
+
+        int number = 1;
+        System.out.println(number);
+        number ++;
+        System.out.println(number);
+        One one = new One();
+
+        System.out.println(one.addOne(number));
+        System.out.println(number);
+    }
+
+    public int addOne(int number) {
+        number ++;
+        return number;
+    }
+}
---
+++ training-solutions/src/main/java/methodpass/WindowOperation.java
@@ -0,0 +1,13 @@
+package methodpass;
+
+import java.util.List;
+
+public class WindowOperation {
+
+    public void riseSize(List<Window> windows, int plusSize) {
+        for(Window w: windows) {
+            w.setHeight(w.getHeight()+plusSize);
+            w.setWidth(w.getWidth()+plusSize);
+        }
+    }
+}
