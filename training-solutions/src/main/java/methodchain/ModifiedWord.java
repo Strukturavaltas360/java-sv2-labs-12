5f9060e916e6b2625fd642803f789763038fba9d
ht0m <hartht@gmail.com>
2021-11-30 11:35:03 +0100

    Methodchain modify words

---
+++ training-solutions/src/main/java/methodchain/ModifiedWord.java
@@ -0,0 +1,10 @@
+package methodchain;
+
+public class ModifiedWord {
+
+    public String modify(String word) {
+        String s = word.substring(2,3);
+        return word.toUpperCase().substring(0,1).concat("x").concat((s.toUpperCase()).concat("y"));
+    }
+    
+}
