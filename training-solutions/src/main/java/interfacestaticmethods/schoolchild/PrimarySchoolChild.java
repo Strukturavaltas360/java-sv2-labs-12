1d5705b23e389c504bd3fed09b08e3f171e3b0a3
ht0m <hartht@gmail.com>
2021-12-29 11:34:19 +0100

    Interface - staticmethods  - Schoolchild

--- training-solutions/src/main/java/interfacestaticmethods/schoolchild/PrimarySchoolChild.java
+++ training-solutions/src/main/java/interfacestaticmethods/schoolchild/PrimarySchoolChild.java
@@ -4,10 +4,10 @@ public interface PrimarySchoolChild {
 
     static PrimarySchoolChild of(int age) {
         if (age > 6 && age < 10) {
-            return new LowerClassSchoolChild("Lacika",6);
+            return new LowerClassSchoolChild("Lacika",age);
         }
         if (age > 11 && age < 14) {
-            return new UpperClassSchoolChild("Jutka", 13);
+            return new UpperClassSchoolChild("Jutka", age);
         }
 
         throw new IllegalArgumentException("It's not a schoolchild with this age!");
