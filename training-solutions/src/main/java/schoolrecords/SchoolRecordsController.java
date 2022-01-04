bc00ccdd52687cc3e88a365b65809041ded5153f
ht0m <hartht@gmail.com>
2022-01-04 11:41:29 +0100

    Classrecords - getMark + validate mark for repetition

--- training-solutions/src/main/java/schoolrecords/SchoolRecordsController.java
+++ training-solutions/src/main/java/schoolrecords/SchoolRecordsController.java
@@ -176,8 +176,7 @@ public class SchoolRecordsController {
             studentForRepetition.grading(new Mark(marktype, subject, tutor));
             System.out.print(FRAME_COLORSCHEME + " " + LINE_INPUT_COLORSCHEME + " Rögzítés SIKERES!");
         } else {
-            System.out.print
-                    (FRAME_COLORSCHEME + " " + LINE_INPUT_COLORSCHEME + " " + tutor.getName() + " nem tanítja a(z) " + subject.getSubjectName() + " tárgyat! - Rögzítés SIKERTELEN!");
+            System.out.print(FRAME_COLORSCHEME + " " + LINE_INPUT_COLORSCHEME + " " + tutor.getName() + " nem tanítja a(z) " + subject.getSubjectName() + " tárgyat! - Rögzítés SIKERTELEN!");
         }
     }
 
