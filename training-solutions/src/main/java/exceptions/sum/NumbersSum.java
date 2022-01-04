497303dd4fe286618fcdea83aba03b953359ff77
ht0m <hartht@gmail.com>
2022-01-04 18:42:35 +0100

    Exception - NumbersSum

---
+++ training-solutions/src/main/java/exceptions/sum/NumbersSum.java
@@ -0,0 +1,31 @@
+package exceptions.sum;
+
+public class NumbersSum {
+
+
+    public int getSum(int[] numbers) {
+        if (numbers.length == 0) {
+            throw new IllegalArgumentException("Null parameter!");
+        }
+        int sum = 0;
+        for (Integer number : numbers) {
+            sum += number;
+        }
+        return sum;
+        }
+
+        public int getSum (String[]numbers){
+            if (numbers.length == 0) {
+                throw new IllegalArgumentException("Null parameter!");
+            }
+            int sum =0;
+            try {
+                for (String s: numbers) {
+                    sum += Integer.parseInt(s);
+                }
+            } catch (IllegalArgumentException iae) {
+                throw new IllegalArgumentException("Not a number", iae);
+            }
+            return sum;
+        }
+    }
---
+++ training-solutions/src/test/java/exceptions/sum/NumbersSumTest.java
@@ -0,0 +1,39 @@
+package exceptions.sum;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumbersSumTest {
+
+    @Test
+    void testNumberSumInteger() {
+        int[] intArray = {10,24,32,0,98};
+        assertEquals(164, new NumbersSum().getSum(intArray));
+    }
+
+    @Test
+    void testNumberSumIntegerWithNull() {
+        int[] intArray = {};
+        NumbersSum numbersSum = new NumbersSum();
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> numbersSum.getSum(intArray));
+        assertEquals("Null parameter!", ex.getMessage());
+    }
+
+    @Test
+    void testNumberSumString() {
+        String[] StringArray = {"4","56","7","23","1"};
+        NumbersSum numbersSum = new NumbersSum();
+        assertEquals(91, numbersSum.getSum(StringArray) );
+    }
+
+    @Test
+    void testNumberSumStringWithNull() {
+        String[] StringArray = {};
+        NumbersSum numbersSum = new NumbersSum();
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> numbersSum.getSum(StringArray));
+        assertEquals("Null parameter!", ex.getMessage());
+    }
+
+}
+
