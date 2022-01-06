ee4aef462b670de3ba20f0ec25c571513eec82a1
ht0m <hartht@gmail.com>
2022-01-06 14:13:16 +0100

    Project 3. Catalog

--- training-solutions/src/main/java/catalog/AudioFeatures.java
+++ training-solutions/src/main/java/catalog/AudioFeatures.java
@@ -1,6 +1,41 @@
+
 package catalog;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AudioFeatures implements Feature{
 
+    private String title;
+    private int length;
+    private List<String> composers = new ArrayList<>();
+    private List<String> performers = new ArrayList<>();
 
+    public AudioFeatures(String title, int length, List<String> composers) {
+        this.title = title;
+        this.length = length;
+        this.composers = composers;
+    }
+
+    public AudioFeatures(String title, int length, List<String> composers, List<String> performers) {
+        this.title = title;
+        this.length = length;
+        this.composers = composers;
+        this.performers = performers;
+    }
+
+    @Override
+    public List<String> getContributors() {
+        return List.copyOf(composers);
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    public int getLength() {
+        return length;
+    }
 }
+
--- training-solutions/src/main/java/catalog/PrintedFeatures.java
+++ training-solutions/src/main/java/catalog/PrintedFeatures.java
@@ -1,4 +1,31 @@
 package catalog;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class  PrintedFeatures implements Feature{
+
+    private String title;
+    private int numberOfPages;
+    private List<String> authors = new ArrayList<>();
+
+    public PrintedFeatures(String title, int numberOfPages, List<String> authors) {
+        this.title = title;
+        this.numberOfPages = numberOfPages;
+        this.authors = authors;
+    }
+
+    @Override
+    public List<String> getContributors() {
+        return List.copyOf(authors);
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
 }
