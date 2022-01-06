364a232cb95925105ac714634d3fa3865b08f5cb
ht0m <hartht@gmail.com>
2022-01-06 18:42:28 +0100

    Project 3. Catalog

--- training-solutions/src/main/java/catalog/PrintedFeatures.java
+++ training-solutions/src/main/java/catalog/PrintedFeatures.java
@@ -3,7 +3,10 @@ package catalog;
 import java.util.ArrayList;
 import java.util.List;
 
-public class  PrintedFeatures implements Feature{
+import static catalog.Validators.isBlank;
+import static catalog.Validators.isEmpty;
+
+public class PrintedFeatures implements Feature {
 
     private String title;
     private int numberOfPages;
@@ -13,6 +16,19 @@ public class  PrintedFeatures implements Feature{
         this.title = title;
         this.numberOfPages = numberOfPages;
         this.authors = authors;
+        validate(title, numberOfPages, authors);
+    }
+
+    private void validate(String title, int numberOfPages, List<String> authors) {
+        if (numberOfPages <= 0) {
+            throw new IllegalArgumentException("Invalid number of pages!");
+        }
+        if (isBlank(title)) {
+            throw new IllegalArgumentException("Empty title");
+        }
+        if (isEmpty(authors)) {
+            throw new IllegalArgumentException("Empty authors");
+        }
     }
 
     @Override
