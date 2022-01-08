14a9c5ca26acfaf6c75bf1f7184b971d46e85d51
ht0m <hartht@gmail.com>
2022-01-08 14:45:04 +0100

    Wit or without generics

---
+++ training-solutions/src/main/java/genericsusage/withoutgenerics/Library.java
@@ -0,0 +1,20 @@
+package genericsusage.withoutgenerics;
+
+import genericsusage.Book;
+
+import java.util.List;
+
+public class Library {
+    public Book getFirstBook(List books) {
+        if (books == null) {
+            throw new NullPointerException("Argument should not be null!");
+        }
+        if (books.size()==0) {
+            throw new IllegalArgumentException("Argument should not be empty!");
+        }
+        if (!(books.get(0) instanceof Book)) {
+            throw new ClassCastException("Not a book");
+        }
+        return (Book) books.get(0);
+    }
+}
