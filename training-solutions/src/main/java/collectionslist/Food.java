df678fdc018e92077fee45428ee87ce4423b3c6b
ht0m <hartht@gmail.com>
2022-01-09 16:58:18 +0100

    Collectionlist - food

---
+++ training-solutions/src/main/java/collectionslist/Food.java
@@ -0,0 +1,17 @@
+package collectionslist;
+
+import java.time.LocalDate;
+
+public class Food {
+
+    private String name;
+    private LocalDate expirationDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+}
