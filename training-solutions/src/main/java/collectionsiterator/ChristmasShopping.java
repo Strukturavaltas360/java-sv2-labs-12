1072e7aed1ed5901e10cf6bb54f0149ac487e54d
ht0m <hartht@gmail.com>
2022-01-14 14:42:50 +0100

    CollectionsIterator - ChristmasShopping

---
+++ training-solutions/src/main/java/collectionsiterator/ChristmasShopping.java
@@ -0,0 +1,27 @@
+package collectionsiterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ChristmasShopping {
+
+    List<ChristmasPresent> christmasPresent = new ArrayList<>();
+
+    public ChristmasShopping(List<ChristmasPresent> christmasPresent) {
+        this.christmasPresent = christmasPresent;
+    }
+
+    public void addNewPresent(ChristmasPresent present) {
+        christmasPresent.add(present);
+    }
+
+    public void removeTooExpensivePresent(int maxPrice) {
+        Iterator<ChristmasPresent> i = christmasPresent.iterator();
+        while (i.hasNext()) {
+            if (i.next().getPrice()>maxPrice) {
+                i.remove();
+            }
+        }
+    }
+}
